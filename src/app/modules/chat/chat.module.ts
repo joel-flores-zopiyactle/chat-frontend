cb0124d09b0ed81282ec22345c87a6176ad91dc3
JoelFloresZ <joelfzop@gmail.com>
2021-11-14 14:49:55 -0600

    UI Desktop  Terminado

--- src/app/modules/chat/chat.module.ts
+++ src/app/modules/chat/chat.module.ts
@@ -1,13 +1,24 @@
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { FormMessageComponent } from './components/form-message/form-message.component';
+import { NavUserComponent } from './components/nav-user/nav-user.component';
+import { NavContactComponent } from './components/nav-contact/nav-contact.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatPageComponent } from './page/chat-page/chat-page.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
 
 
 @NgModule({
   declarations: [
-    ChatPageComponent
+    ChatPageComponent,
+    ChatListComponent,
+    NavContactComponent,
+    NavUserComponent,
+    FormMessageComponent,
+    ContactInfoComponent
+
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/chat/components/nav-contact/nav-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-contact',
+  templateUrl: './nav-contact.component.html',
+  styleUrls: ['./nav-contact.component.css']
+})
+export class NavContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
